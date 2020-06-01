41b7677219e083b4d10badd2b9dc35e741278211
Princeton Arulnesan <princeton.arul@gmail.com>
2020-06-01 18:58:21 +0000

    updates to files

--- server.js
+++ server.js
@@ -35,19 +35,21 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const favouritesRoutes = require("./routes/favourites")
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
+// app.use("/api/favourites", favouritesRoutes(awer));
 
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("favourites");
 });
 
 app.listen(PORT, () => {
