1b3d56ad8da59f4a5a1166610623651be523b802
John Kim <kimjon0509@gmail.com>
2020-06-01 18:08:48 +0000

    changed buy and sell tab

--- routes/messages.js
+++ routes/messages.js
@@ -24,16 +24,26 @@ module.exports = (db, io) => {
     .then(data => res.json(data.rows))
   })
 
-  router.get("/:user_id", (req, res) => {
-    console.log(req.query)
+  router.get("/buy", (req, res) => {
+    console.log(req.query, 'buy')
+    db.query(`
+      SELECT messages.id, messages.content, messages.seller_id, users.name as user_name, users.profile_photo, products.name as product_name, products.main_photo, products.price
+      FROM messages
+      JOIN users ON users.id = buyer_id
+      JOIN products ON products.id = product_id
+      WHERE messages.buyer_id = ${req.query.buyer_id}`)
+    .then(data => res.json(data.rows));
+  });
+
+  router.get("/sell", (req, res) => {
+    console.log(req.query.seller_id, 'sell')
     db.query(`
       SELECT messages.id, messages.content, messages.buyer_id, users.name as user_name, users.profile_photo, products.name as product_name, products.main_photo, products.price
       FROM messages
       JOIN users ON users.id = buyer_id
       JOIN products ON products.id = product_id
-      WHERE messages.seller_id = ${req.params.user_id}`)
-    .then(data => res.json(data.rows));
+      WHERE messages.seller_id = ${req.query.seller_id}`)
+    .then(data => {console.log('working'); res.json(data.rows)});
   });
-
   return router;
 };
