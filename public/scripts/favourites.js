f6fe306bae5ee5574715d443d0adaf70a418f551
Princeton Arulnesan <princeton.arul@gmail.com>
2020-06-02 20:27:24 +0000

    fixed up favourites.js

--- public/scripts/favourites.js
+++ public/scripts/favourites.js
@@ -1,41 +1,6 @@
 const renderFavouritesPage = () => {
   return $('.main-content').append(`
   <section class="favourites-body">
-  <h3> Filters </h3>
-  <section class="filters">
-    <form method="POST">
-    <input type="integer" placeholder="Minimum Price"></input>
-  <input type="integer" placeholder="Maximum Price"></input>
-</section>
-<section class="filters">
-   <div class="custom-select">
-  <select>
-    <option value="0"> Select Category:</option>
-    <option value="1"> Phone </option>
-    <option value="2"> Computer </option>
-    <option value="3"> T.V </option>
-    <option value="4"> Camera </option>
-    <option value="5"> Video Games </option>
-    <option value="6"> Headphones </option>
-    <option value="7"> Printer </option>
-    <option value="8"> Tablet </option>
-    <option value="9"> Laptop </option>
-     </select>
-   </div>
-<div class="custom-select">
-  <select>
-    <option value="0"> Select Condition: </option>
-    <option value="1"> Very Bad </option>
-    <option value="2"> Bad </option>
-    <option value="3"> Good </option>
-    <option value="4"> Very Good </option>
-    <option value="5"> Excellent </option>
-    <option value="6"> Brand-new </option>
-     </select>
-   </div>
-</section>
-<button type="submit" class="filter-button">Submit</button>
-</form>
   <h1 class="title">Favourites</h1>
   <div class="boxes"><img class="img-favourites" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Arduino_ftdi_chip-1.jpg/1920px-Arduino_ftdi_chip-1.jpg">
     <p class="information"> information </p>
